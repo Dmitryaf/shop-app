09b468d87478bda1a5eed963d8604b5e9f5d148f
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-08 13:40:57 +0500

    Added Product component

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addToCart } from "../../redux/dataReducer";
+import "./Product.scss";
+
+function Product(props) {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.dataReducer.cart);
+
+  const { data } = props;
+  const { gameID: id, title, normalPrice: price, thumb: img } = data;
+
+  const addCart = (productId) => {
+    dispatch(addToCart(productId));
+  };
+
+  return (
+    <div className="product">
+      <img src={img} alt={title} className="product__img" />
+      <h2 className="product__title">{title}</h2>
+      <div className="product__price">{price} $</div>
+      <button type="button" onClick={() => addCart(id)} className="btn btn_w100">
+        Add to cart
+      </button>
+    </div>
+  );
+}
+
+export default Product;

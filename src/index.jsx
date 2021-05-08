707738f1cb8b086adb589bd51432b985297300ee
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-08 13:41:13 +0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 import "./styles/global.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );

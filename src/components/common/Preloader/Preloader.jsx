b1856004ccc35a4ddab1421678d1d9f067dd8890
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-08 13:41:07 +0500

    Added Preloader component

---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Preloader.scss";
+
+function Preloader() {
+  const contentContainer = <div className="preloader"> </div>;
+  return contentContainer;
+}
+
+export default Preloader;

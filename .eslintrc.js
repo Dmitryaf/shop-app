7cd86a809176fba4590d371709817235e468fa75
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-08 13:27:55 +0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,29 @@
 module.exports = {
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:prettier/recommended',
-    'react-app',
-  ],
-  plugins: ['react'],
+  extends: ["airbnb", "airbnb/hooks", "plugin:prettier/recommended", "react-app"],
+  plugins: ["react"],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
-    'linebreak-style': 'off',
-    'prettier/prettier': [
-      'error',
+    "linebreak-style": "off",
+    "react/prop-types": "off",
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
+        endOfLine: "auto",
       },
     ],
   },

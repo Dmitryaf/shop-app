7d96e3097f7e26811b985a55b4f10bd576ca7831
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-10 14:16:35 +0500

    Added conditions for disabling the button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,21 +6,24 @@ import "./Product.scss";
 
 function Product(props) {
   const dispatch = useDispatch();
+  const cart = useSelector((state) => state.dataReducer.cart);
 
   const { data } = props;
-  const { gameID: id, title, normalPrice: price, thumb: img } = data;
+  const { id, title, price, image } = data;
 
-  const addCart = (productId) => {
-    dispatch(addToCart(productId));
+  const inCart = cart.find((item) => item.id === id);
+
+  const addCart = () => {
+    dispatch(addToCart(id));
   };
 
   return (
     <div className="product">
-      <img src={img} alt={title} className="product__img" />
+      <img src={image} alt={title} className="product__img" />
       <h2 className="product__title">{title}</h2>
-      <div className="product__price">{price} $</div>
-      <button type="button" onClick={() => addCart(id)} className="btn btn_w100">
-        Add to cart
+      <div className="product__price">{price}$</div>
+      <button type="button" onClick={addCart} className={`btn ${inCart ? "btn_disabled" : ""}`}>
+        {inCart ? "Added to cart" : "Add to cart"}
       </button>
     </div>
   );

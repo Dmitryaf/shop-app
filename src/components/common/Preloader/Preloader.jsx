661ec3e4b9776610b7cf6ba8f28107f2c01bd5c9
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-10 14:12:41 +0500

    Deleted

--- src/components/common/Preloader/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Preloader.scss";
-
-function Preloader() {
-  const contentContainer = <div className="preloader"> </div>;
-  return contentContainer;
-}
-
-export default Preloader;

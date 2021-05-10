5164187600aadc24cf8621a85f58d94d3e0e1b13
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-05-10 14:13:32 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,15 @@
 import axios from "axios";
-import { setCardType, setData, toggleIsFetching } from "../redux/dataReducer";
 
-export function getApiData() {
-  const apiUrl = "https://www.cheapshark.com/api/1.0/deals?pageSize=10&";
+import { setCardType } from "../redux/dataReducer";
 
-  return async (dispatch) => {
-    try {
-      const response = await axios(`${apiUrl}`);
-      const data = await response.data;
-      dispatch(toggleIsFetching(false));
-      dispatch(setData(data));
-      dispatch(toggleIsFetching(true));
-    } catch (e) {
-      throw new Error(e);
-    }
-  };
-}
-
-export function getCardType(inputValue) {
+function getCardType(inputValue) {
   const apiUrl = `https://api.cardinfo.online?input=${inputValue}&apiKey=df64d04456a001cbeb54367c948742b3`;
 
   return async (dispatch) => {
     try {
-      const response = await axios(`${apiUrl}`);
+      const response = await axios(`${apiUrl}`, {
+        headers: { "Access-Control-Allow-Headers": "*" },
+      });
       const data = await response.data;
       dispatch(setCardType(data));
     } catch (e) {
@@ -30,3 +17,5 @@ export function getCardType(inputValue) {
     }
   };
 }
+
+export default getCardType;
